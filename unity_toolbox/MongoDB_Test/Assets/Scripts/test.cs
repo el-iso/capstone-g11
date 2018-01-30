57bac7ae09d9b483009eed3efb428a47f762c902
DerekPaschal <dtpaschal@gmail.com>
2018-01-30 17:25:46 -0600

    Modifications to MongoDB_Test
    MongoDB_Test modifications

--- unity_toolbox/MongoDB_Test/Assets/Scripts/test.cs
+++ unity_toolbox/MongoDB_Test/Assets/Scripts/test.cs
@@ -13,6 +13,24 @@ public class test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Connect directly to a single MongoDB server
+        var client = new MongoClient("mongodb://localhost:27017");
+
+        // Get DB and collection
+        var database = client.GetDatabase("randomdata");
+        var collection = database.GetCollection<BsonDocument>("randomTest");
+
+        //// Set document filter (find document with given field-value pair)
+        //var filter = Builders<BsonDocument>.Filter.Eq("role", "test2");
+        //var sort = Builders<BsonDocument>.Sort.Descending("role");
+
+        //// Set projection
+        //var projection = Builders<BsonDocument>.Projection.Exclude("_id");
+
+        //// Search for document with given parameters
+        //var document = collection.Find(new BsonDocument()).Project(projection).First();
+
+        //// Print the retrieved document to the Unity debug console
+        //Debug.Log(document.ToString());
+    }
 }

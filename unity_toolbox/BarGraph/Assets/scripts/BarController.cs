95dc8bf5b73ae874c3f8c6132dfb50465bfda02c
el-iso <elijamesbarton@gmail.com>
2018-01-30 15:57:09 -0600

    created BarGraph project in toolbox and started experimenting

---
+++ unity_toolbox/BarGraph/Assets/scripts/BarController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.localScale += new Vector3(0, 1.0f, 0);
+	}
+}

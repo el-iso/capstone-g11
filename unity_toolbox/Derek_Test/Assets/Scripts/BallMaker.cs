b6fc02e7846352625e139c32f7b45f38d0fd2ecb
DerekPaschal <dtpaschal@gmail.com>
2018-02-12 14:53:38 -0600

    Made a popcorn popper
    I made a popcorn popper woo

---
+++ unity_toolbox/Derek_Test/Assets/Scripts/BallMaker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMaker : MonoBehaviour {
+
+    public GameObject spawn;
+    [Range(0,1)]
+    public float frequency = 0.5f;
+    [Range(0, 1000)]
+    public int numBalls = 100;
+    [Range(0, 5)]
+    public float radius = 1.0f;
+
+    private Transform tr;
+    private int timer;
+    private int ballsSpawned;
+
+	// Use this for initialization
+	void Start () {
+        this.tr = this.gameObject.transform;
+        this.timer = 0;
+        this.ballsSpawned = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer++;
+        float secs_elapsed = timer / 60.0f;
+        if (secs_elapsed > frequency)
+        {
+            timer = 0;
+            if (ballsSpawned <= numBalls)
+            {
+                GameObject obj = Instantiate(spawn, new Vector3(this.tr.position.x + Random.Range(0, this.radius) - (this.radius/2.0f), this.tr.position.y, this.tr.position.z + Random.Range(0, this.radius) - (this.radius / 2.0f)), Quaternion.identity);
+                obj.GetComponent<SelfDescruct>().range = 15.0f;
+            }
+        }
+	}
+}
--- unity_toolbox/Derek_Test/Assets/Scripts/RotationSetDial.cs
+++ unity_toolbox/Derek_Test/Assets/Scripts/RotationSetDial.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotationSetDial : MonoBehaviour {
 
+    [Range(0, 2)]
+    public int Heart_BloodOX_Resp;
+
     private Transform tr;
     private Vector3 startRotation;
 
@@ -16,7 +19,24 @@ public class RotationSetDial : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Get some data value here
-        float data_value = 100;
+        //float data_value = 100;
+        float data_value = 0;
+
+        switch (this.Heart_BloodOX_Resp)
+        {
+            case 0:
+                data_value = MongoInterface.GetHeartbeat();
+                break;
+            case 1:
+                data_value = MongoInterface.GetBloodOxygen();
+                break;
+            case 2:
+                data_value = MongoInterface.GetRespiration();
+                break;
+            default:
+                data_value = MongoInterface.GetHeartbeat();
+                break;
+        }
 
         Vector3 newRotation = this.startRotation;
         newRotation.y += data_value;

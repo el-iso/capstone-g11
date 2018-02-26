8d919051ca95f922c7cfbd8926747e180573c0d0
StellarHorizon <ztjohnson34@yahoo.com>
2018-02-26 14:44:36 -0600

    Temporary ControlCenter Upload
    Unfinished ControlCenter scene upload to save progress.

---
+++ unity_toolbox/Projects/Zach-Temp-ControlCenter/UI_Test2D/Assets/ControlCenter/PopulateHeaderFields.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateHeaderFields : MonoBehaviour
+{
+    public GameObject FieldPrefab; // This is our prefab object that will be exposed in the inspector
+    public string[] testArray = { "Status", "Disable Visual", "Radio Name", "Last Received", "View" };
+
+    //public int numberToCreate; // number of objects to create. Exposed in inspector
+
+    void Start()
+    {
+        Populate();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Populate()
+    {
+        GameObject fieldObj; // Create GameObject instance
+        Text fieldText;
+
+        for (int i = 0; i < testArray.Length; i++)
+        {
+            // Create new instances of our prefab until we've created as many as we specified
+            fieldObj = (GameObject)Instantiate(FieldPrefab, transform);
+
+            // Set text field info
+            fieldText = fieldObj.GetComponent<Button>().transform.Find("Text").GetComponent<Text>();
+            fieldText.text = testArray[i];
+            fieldText.fontSize = 20;
+        }
+
+    }
+}
---
+++ unity_toolbox/Projects/Zach-Temp-ControlCenter/UI_Test2D/Assets/ControlCenter/ViewGraphBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewGraphBehavior : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Button>().transform.Find("Text").GetComponent<Text>().text = ">";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Button>().transform.Find("Text").GetComponent<Text>().text = ">";
+    }
+}

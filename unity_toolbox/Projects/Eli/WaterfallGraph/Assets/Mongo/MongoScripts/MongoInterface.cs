caa9cb2e0d8adf40be6457c3081df0f919bdd716
el-iso <elijamesbarton@gmail.com>
2018-02-26 12:00:27 -0600

    Updated mongo package within Waterfall Graph

--- unity_toolbox/Projects/Eli/WaterfallGraph/Assets/Mongo/MongoScripts/MongoInterface.cs
+++ unity_toolbox/Projects/Eli/WaterfallGraph/Assets/Mongo/MongoScripts/MongoInterface.cs
@@ -1,24 +1,29 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Bson;
 using UnityEngine;
-using System;
-using System.Text;
-using System.Collections.Generic;
+using MongoDB.Driver.Builders;
 
 public class MongoInterface : MonoBehaviour {
 
+    //Inspector Window
+    public float HeartBeat;
+    public float ResperationRate;
+    public float BloodOX;
+
+    //Configuration Variables
     public static string database = "randomdata";
     public static string collection = "randomTest";
     public static float poll_interval = 1.0f;
 
     // Database variables
     private static MongoClient Client { get; set; }
-    private static IMongoDatabase Database { get; set; }
-    private static IMongoCollection<BsonDocument> Collection { get; set; }
+    private static MongoServer Server { get; set; }
+    private static MongoDatabase Database { get; set; }
+    private static MongoCollection<BsonDocument> Collection { get; set; }
 
-    private static float heartbeat = 100.0f;
-    private static float respiration = 100.0f;
-    private static float bloodOxygen = 100.0f;
+    private static float heartbeat = -1.0f;
+    private static float respiration = -1.0f;
+    private static float bloodOxygen = -1.0f;
 
     private static float time_of_last_poll = 0.0f;
 
@@ -28,21 +33,21 @@ public class MongoInterface : MonoBehaviour {
         EstablishConnection("mongodb://localhost:27017", database, collection);
     }
 
-    // Unity Function : Update is called once per frame
-    void Update() {
-        
-    }
-
     //Called once per fixed physics time step
     void FixedUpdate()
     {
         if (Time.time - time_of_last_poll >= poll_interval)
         {
-            var results = SearchRecentByDeviceID(0, 1);
-            heartbeat = float.Parse(results[0]["h"].ToString());
-            respiration = float.Parse(results[0]["r"].ToString());
-            bloodOxygen = float.Parse(results[0]["b"].ToString());
-            print(heartbeat);
+            BsonDocument results = SearchRecentByDeviceID(0, 1);
+            heartbeat = float.Parse(results["h"].ToString());
+            respiration = float.Parse(results["r"].ToString());
+            bloodOxygen = float.Parse(results["b"].ToString());
+
+            //Set Inspector Window Variables
+            this.HeartBeat = MongoInterface.heartbeat;
+            this.ResperationRate = MongoInterface.respiration;
+            this.BloodOX = MongoInterface.bloodOxygen;
+
             time_of_last_poll = Time.time;
         }
     }
@@ -84,94 +89,9 @@ public class MongoInterface : MonoBehaviour {
     {
         // Set database variables
         Client = new MongoClient(connectionString);
-        Database = Client.GetDatabase(dbName);
+        Server = Client.GetServer();
+        Database = Server.GetDatabase(dbName);
         Collection = Database.GetCollection<BsonDocument>(collectionName);
-    }
-
-    /// <summary>
-    /// Drops the database passed as a parameter. Returns true if database dropped, false otherwise.
-    /// </summary>
-    /// <param name="dbName"></param>
-    public static bool DropDatabase(string dbName)
-    {
-        // Check if database exists
-        if (Client.GetDatabase(dbName) == null)
-            return false;
-
-        // Remove old references
-        if (String.Equals(dbName, Database.DatabaseNamespace.DatabaseName))
-        {
-            // Remove Collection reference if within database
-            if (System.Object.ReferenceEquals(Collection, Database.GetCollection<BsonDocument>(Collection.CollectionNamespace.CollectionName)))
-                Collection = null;
-            Database = null;
-        }
-
-        // Drop database
-        Client.DropDatabase(dbName);
-
-        // Verify database was dropped
-        if (Client.GetDatabase(dbName) == null)
-            return true;
-        else
-            return false;
-    }
-
-    /// <summary>
-    /// Drops the collection in the current database. Returns true if collection dropped, false otherwise.
-    /// </summary>
-    /// <param name="dbName"></param>
-    public static bool DropCollection(string collectionName)
-    {
-        return DropCollection(Database.DatabaseNamespace.DatabaseName, collectionName);
-    }
-
-    /// <summary>
-    /// Drops the collection in the specified database. Returns true if collection dropped, false otherwise.
-    /// </summary>
-    /// <param name="dbName"></param>
-    public static bool DropCollection(string dbName, string collectionName)
-    {
-        var DB = Client.GetDatabase(dbName);
-
-        // Check if collection exists
-        if (DB == null || DB.GetCollection<BsonDocument>(collectionName) == null)
-            return false;
-
-        // Remove reference if same as Collection
-        if (String.Equals(collectionName, DB.DatabaseNamespace.DatabaseName))
-            Collection = null;
-
-        // Drop collection
-        DB.DropCollection(collectionName);
-
-        // Verify collection was dropped
-        if (DB.GetCollection<BsonDocument>(collectionName) == null)
-            return true;
-        else
-            return false;
-    }
-
-    /// <summary>
-    /// Insert a JSON-encoded document into the current database and collection.
-    /// </summary>
-    /// <param name="dbName"></param>
-    /// <param name="collectionName"></param>
-    /// <param name="json"></param>
-    private static async void InsertDocument(string json)
-    {
-        await Collection.InsertOneAsync(BsonDocument.Parse(json));
-    }
-
-    /// <summary>
-    /// Insert a JSON-encoded document into the specified database and collection.
-    /// </summary>
-    /// <param name="dbName"></param>
-    /// <param name="collectionName"></param>
-    /// <param name="json"></param>
-    private static async void InsertDocument(string dbName, string collectionName, string json)
-    {
-        await Client.GetDatabase(dbName).GetCollection<BsonDocument>(collectionName).InsertOneAsync(BsonDocument.Parse(json));
     }
 
     /// <summary>
@@ -181,11 +101,21 @@ public class MongoInterface : MonoBehaviour {
     /// <param name="deviceID"></param>
     /// <param name="limit"></param>
     /// <returns></returns>
-    private static List<BsonDocument> SearchRecentByDeviceID(int deviceID, int limit = 10000)
+    private static BsonDocument SearchRecentByDeviceID(int deviceID, int limit = 1)
     {
-        var filter = Builders<BsonDocument>.Filter.Empty;
-        var sort = Builders<BsonDocument>.Sort.Descending("sent");
-        var result = Collection.Find(filter).Limit(limit).Sort(sort).ToList();
+        //var filter = Builders<BsonDocument>.Filter.Empty;
+        //var sort = Builders<BsonDocument>.Sort.Descending("sent");
+        //var result = Collection.Find(filter).Limit(limit).Sort(sort).ToList();
+
+        var query = new QueryDocument("did", deviceID);
+        BsonDocument result = new BsonDocument();
+        foreach(BsonDocument doc in Collection.Find(MongoDB.Driver.Builders.Query.EQ("did",deviceID)).SetLimit(limit).SetSortOrder(SortBy.Descending("sent")))
+        {
+            result = doc;
+            break;
+        }
+        
+        
         return result;
     }
 }
